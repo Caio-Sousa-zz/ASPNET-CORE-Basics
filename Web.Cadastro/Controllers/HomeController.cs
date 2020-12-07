262f1259a33e82da915e58e85f288465c259ad2c
Caio César Silva Sousa <caiyo.projeto@gmail.com>
2020-12-07 09:49:25 -0300

    Inclusão de rotas inteligentes

--- Web.Cadastro/Controllers/HomeController.cs
+++ Web.Cadastro/Controllers/HomeController.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Web.Cadastro.Models;
 
 namespace Web.Cadastro.Controllers
 {
+    [Route("")]
+    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -18,17 +17,23 @@ namespace Web.Cadastro.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid?}")]
+        public IActionResult Index(int id, Guid categoria)
         {
             return View();
         }
 
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- Web.Cadastro/Startup.cs
+++ Web.Cadastro/Startup.cs
@@ -23,7 +23,7 @@ namespace Web.Cadastro
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,8 +46,17 @@ namespace Web.Cadastro
 
             app.UseAuthorization();
 
+            //https://docs.microsoft.com/en-us/aspnet/core/fundamentals/routing?view=aspnetcore-5.0
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                 name: "modulos",
+                 pattern: "Gestao/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                  name: "categoria",
+                  pattern: "{controller=Home}/{action=Index}/{id}/{categoria?}");
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
