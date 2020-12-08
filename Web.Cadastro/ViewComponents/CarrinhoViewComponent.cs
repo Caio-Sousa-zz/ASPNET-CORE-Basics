1803eafd0ef0485cd86a0e1d0b8e99989766608c
Caio César Silva Sousa <caiyo.projeto@gmail.com>
2020-12-08 17:48:38 -0300

    Include View Component

---
+++ Web.Cadastro/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Cadastro.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int ItensCarrinho { get; set; }
+
+        public CarrinhoViewComponent()
+        {
+            ItensCarrinho = 10;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ItensCarrinho);
+        }
+    }
+}
