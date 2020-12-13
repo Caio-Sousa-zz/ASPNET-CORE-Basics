fbb166e35890d02f9c8176779754fb1f1b0e7f4b
Caio César Silva Sousa <caiyo.projeto@gmail.com>
2020-12-13 18:16:23 -0300

    Basic View and Controller with webpage.

--- src/Dev.Web/Startup.cs
+++ src/Dev.Web/Startup.cs
@@ -1,12 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Dev.Web
 {
@@ -16,6 +11,7 @@ namespace Dev.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +24,11 @@ namespace Dev.Web
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseEndpoints(endPoints => {
+                endPoints.MapControllerRoute(
+                    name: "modulos",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
