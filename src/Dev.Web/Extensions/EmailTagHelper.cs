8ea2af59c4dcc322fbd78e4740266ad51ab68470
Caio César Silva Sousa <caiyo.projeto@gmail.com>
2020-12-14 00:13:19 -0300

    Include tag Helper

---
+++ src/Dev.Web/Extensions/EmailTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace Dev.Web.Extensions
+{
+    public class EmailTagHelper: TagHelper
+    {
+        public string EmailDomain { get; set; } = "gmail.helper.com";
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            // Link
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
--- src/Dev.Web/wwwroot/js/Site.js
+++ src/Dev.Web/wwwroot/js/Site.js
@@ -1,1 +1,3 @@
-﻿
+﻿$(document).ready(function () {
+    alert("carregado!");
+})
